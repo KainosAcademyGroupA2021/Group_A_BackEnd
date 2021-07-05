2d093da6369685c647989e54ff91d6b749f91937
Joe Downard <joseph.downard@kainos.com>
2021-07-05 14:48:03 +0100

    Basic backend setup

---
+++ backend/dbconnection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql')
+const dbconfig = require('./dbconfig.json')
+const util = require('util')
+
+function wrapDB(dbconfig) {
+    const pool = mysql.createPool(dbconfig)
+    return {
+        query(sql, args) {
+            return util.promisify(pool.query)
+                .call(pool, sql, args)
+        },
+        release() {
+            return util.promisify(pool.releaseConnection)
+                .call(pool)
+        }
+    }
+}
+
+const db = wrapDB(dbconfig);
