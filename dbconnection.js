e9b96e9232a4b07a5f827030959d3b0417861dca
Aleksandra <aleksandra.bogusz@kainos.com>
2021-07-06 12:59:10 +0200

    use-case-2

--- dbconnection.js
+++ dbconnection.js
@@ -1,6 +1,14 @@
 const mysql = require('mysql')
 const dbconfig = require('./dbconfig.json')
+const express = require("express");
+const cors = require('cors');
+const app = express()
 const util = require('util')
+
+const bodyParser = require("body-parser");
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 function wrapDB(dbconfig) {
     const pool = mysql.createPool(dbconfig)
@@ -19,6 +27,13 @@ function wrapDB(dbconfig) {
 const db = wrapDB(dbconfig);
 
 exports.getPeopleList = async () => {
-    let results = await db.query('SELECT * FROM JobRoleDatabase.Persons;') 
+    let results = await db.query('SELECT * FROM JobRoleDatabase.Persons;')
     return results;
+}
+
+exports.getJobRolesSpecifications = async (name) => {
+    console.log(name);
+    let results = await db.query(`SELECT RoleName, RoleSpec FROM JobRoleDatabase.Role where RoleName = '${name}' `)
+    return results;
+    
 }
--- routes.js
+++ routes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router()
 const dbconnection = require('./dbconnection.js');
+const cors = require('cors');
+
+const bodyParser = require("body-parser");
+
+router.use(express.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+
+router.use(cors());
+
 
 router.get("/people-list", async (req, res) => {
     res.json(await dbconnection.getPeopleList());
@@ -8,6 +17,11 @@ router.get("/people-list", async (req, res) => {
 
 router.get("/", (req, res) => {
     res.json({hello: "world"});
+});
+
+router.get("/job-roles", async (req, res) => {
+    console.log(req.query.name)
+    res.json(await dbconnection.getJobRolesSpecifications(req.query.name));
 });
 
 module.exports = router;
