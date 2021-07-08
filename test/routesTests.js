1e3b5e0a627b5b14c65d55a246723dce9f1671cb
Maria Bennett <maria.bennett@kainos.com>
2021-07-08 13:23:17 +0100

    use case 5 tests

--- test/routesTests.js
+++ test/routesTests.js
@@ -31,3 +31,25 @@ describe("Job roles testing", () => {
 
 
 })
+describe("Band Competencies testing", () => {
+  it("/getBandCompetencies return list of bands", done=> {
+    request(app)
+    .get("/getBandCompetencies")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then(response => {
+      console.log(response.body)
+      
+      assert(response.body[0], {
+        BandName: 'Trainee',
+        BandLevel: 7,
+        CompetenciesName: 'Communication & influence, Personal performance, Working with others, Setting direction development & accountability, Supporting & delivering strategy, Commerciality & risk'
+      })
+      done();
+    })
+    .catch(err => done(err))
+  
+  })
+
+
+})
