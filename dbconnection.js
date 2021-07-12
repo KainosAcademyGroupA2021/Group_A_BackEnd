399e2f0706592e5a9ea5e99df5d3c65a39129f1a
Joe Downard <joseph.downard@kainos.com>
2021-07-12 09:42:29 +0100

    Fixed tests to match database structure and contents

--- dbconnection.js
+++ dbconnection.js
@@ -54,7 +54,7 @@ exports.getCapabilityAndJobFamily = async () => {
 
 
 exports.getTraingByBand = async() => {
-    let response = await db.query('Select BandID, BandLevel, TrainingType,  BandName, TrainingName, TrainingLink FROM JobRoleDatabase.Band Join Training Using (TrainingID);')
+    let response = await db.query('Select BandID, BandLevel, TrainingType,  BandName, TrainingName, TrainingLink FROM JobRoleDatabase.Band Join Band_Training USING (BandID) JOIN Training Using (TrainingID);')
     return response;
 }
 
