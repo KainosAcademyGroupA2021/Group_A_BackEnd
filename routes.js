f9e2f6486d3bfe57c2e994935badadcf57670293
Aleksandra <aleksandra.bogusz@kainos.com>
2021-07-19 14:26:31 +0200

    adding validation

--- routes.js
+++ routes.js
@@ -51,7 +51,7 @@ router.get("/getJobFamilies", async (req, res) => {
 })
 
 
-router.get("/getCapabilities", async (req, res) => {
+router.get("/getCapabilities", checkJwt, checkScopes, async (req, res) => {
   // #swagger.description = 'gets all capabilities and returns CapabilityID, CapabilityName, CapabilityLeadID'
     res.json(await dbconnection.getCapabilities());
 })
